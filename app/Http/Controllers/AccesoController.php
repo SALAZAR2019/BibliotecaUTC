e5a493765b974e423742bc793bc7cd72217ee4f9
Silvia Canul <silicanulchi16@gmail.com>
2021-09-07 02:01:59 -0500

    Login
    vista y controlador del login, modificación en el master.

---
+++ app/Http/Controllers/AccesoController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User_login;
+use Illuminate\Support\Facades\Redirect;
+use Session;
+use Cache;
+
+class AccesoController extends Controller
+{
+    public function validar(Request $request)
+    {
+        //usuario y password son los name del formulario login
+        $user = $request->get('usuario');
+        $contraseña = $request->get('password');
+
+        $ingresar = User_login::where('usuario','=', $user)
+        ->where('password','=', $contraseña)
+        ->first();
+
+        if (!empty($ingresar))
+        {
+            $rol=$ingresar->rol_puesto;
+            $nombre = $ingresar->nombres;
+            Session::put('nombre', $nombre);
+            Session::put('rol', $rol);
+
+            if ($rol == 'Bibliotecario'){
+                return redirect('prestamo');
+            }else {
+                return redirect('user');
+            }
+        }else {
+            return redirect('login');
+        }
+    }
+    public function salir()
+    {
+        Session::flush();
+        Session::reflash();
+        Cache::flush();
+        unset($_SESSION);
+        return Redirect::to('login');
+    }
+}
