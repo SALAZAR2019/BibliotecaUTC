23125e6f8b3602517ac0636331468aa7fae92a13
Nataly <33634824+NatalyJimenez@users.noreply.github.com>
2021-09-24 09:58:44 -0500

    cambios css

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -34,14 +34,7 @@
       </li>
     </ul>
 
-    <!-- SEARCH FORM -->
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-navbar" type="submit">
-            <i class="fas fa-search"></i>
-          </button>
-    </form>
-
+    
     <!-- Right navbar links -->
  
   </nav>
@@ -206,13 +199,7 @@
     <!-- /.content -->
   </div>
   <!-- /.content-wrapper -->
-  <footer class="main-footer">
-    <strong>Copyright &copy; 2014-2019 <a href="http://adminlte.io">AdminLTE.io</a>.</strong>
-    All rights reserved.
-    <div class="float-right d-none d-sm-inline-block">
-      <b>Version</b> 3.0.4
-    </div>
-  </footer>
+
 
   <!-- Control Sidebar -->
   <aside class="control-sidebar control-sidebar-dark">
