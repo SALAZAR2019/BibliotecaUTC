664e081ec7a63dd744fb58093407b170131d6532
Nataly <33634824+NatalyJimenez@users.noreply.github.com>
2021-09-24 10:53:08 -0500

    css bar

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -72,8 +72,32 @@
             <a href="#" class="nav-link active">
               <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>
-                Dashboard
-                <i class="right fas fa-angle-left"></i>
+                libro 
+               
+              </p>
+            </a>
+            <li class="nav-item has-treeview menu-open">
+            <a href="#" class="nav-link active">
+              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p>
+                Prestamos 
+               
+              </p>
+            </a>
+            <li class="nav-item has-treeview menu-open">
+            <a href="#" class="nav-link active">
+              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p>
+                Devoluciones 
+               
+              </p>
+            </a>
+            <li class="nav-item has-treeview menu-open">
+            <a href="#" class="nav-link active">
+              <i class="nav-icon fas fa-tachometer-alt"></i>
+              <p>
+                libro 
+               
               </p>
             </a>
 
